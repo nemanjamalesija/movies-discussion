3bf7704cf95cc9865b96a5acfbad17fe2da1eba0
nemanjamalesija <malesija13@gmail.com>
2023-04-20 21:11:15 +0200

    Set app configuration and storage

--- server/app.ts
+++ server/app.ts
@@ -1,1 +1,39 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import dotenv from 'dotenv';
+import express from 'express';
+import { fileURLToPath } from 'url';
 
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__fileName);
+
+dotenv.config();
+const app = express();
+
+/* CONFIG  */
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/assets', express.static(path.join(__dirName, 'public/assets')));
+
+/* STORAGE */
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/assets');
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+export { app };
