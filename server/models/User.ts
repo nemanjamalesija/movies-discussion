9731ce262e3ac4aa9aa05755b14de90cc69d041d
nemanjamalesija <malesija13@gmail.com>
2023-07-17 21:38:29 +0200

    Add starting user model

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,45 +1,56 @@
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-      min: 2,
-      max: 50,
-    },
-    lastName: {
-      type: String,
-      required: true,
-      min: 2,
-      max: 50,
-    },
-    email: {
-      type: String,
-      required: true,
-      max: 50,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      min: 5,
-      unique: true,
-    },
-    picturePath: {
-      type: String,
-      default: '',
-    },
-    friends: {
-      type: Array,
-      default: [],
-    },
-    location: String,
-    occupation: String,
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    requred: [true, 'User must have a name'],
   },
-  { timestamps: true }
-);
 
-const User = mongoose.model('User', UserSchema);
+  email: {
+    type: String,
+    required: [true, 'User must have an email'],
+    unique: true,
+    lowercase: true,
+  },
+
+  photo: {
+    type: String,
+    default: () => {
+      return 'https://jumbo-bowls.onrender.com/public/images/users/defaultUser.webp';
+    },
+  },
+
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+
+  password: {
+    type: String,
+    required: [true, 'User must have a password'],
+    minlength: 1,
+    select: false,
+  },
+
+  passwordConfirm: {
+    type: String || undefined,
+    required: [true, 'You must confirm the password'],
+  },
+
+  joinedAt: {
+    type: Date,
+    default: () => {
+      return Date.now();
+    },
+  },
+
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
 
 export default User;
