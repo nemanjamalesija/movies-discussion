1677a5545a8325d40d1bb2636973148f4b1d363d
nemanjamalesija <malesija13@gmail.com>
2023-07-17 21:25:57 +0200

    Clean code

--- server/app.ts
+++ server/app.ts
@@ -7,10 +7,6 @@ import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
 import { fileURLToPath } from 'url';
-import registerController from './controllers/registerController.ts';
-import verifyToken from './middleware/verifyToken.ts';
-import authRouter from './routes/authRouter.ts';
-import usersRouter from './routes/usersRouter.ts';
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
@@ -27,25 +23,5 @@ app.use(bodyParser.json({ limit: '30mb' }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirName, 'public/assets')));
-
-/* STORAGE */
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/assets');
-  },
-
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
-/*ROUTES WITH FILES */
-app.post('/auth/register', upload.single('picture'), registerController);
-
-/* ROUTES */
-app.use('/auth', authRouter);
-app.use('/users', usersRouter);
 
 export { app };
