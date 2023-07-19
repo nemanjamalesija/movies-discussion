29992084ccbea5d680979fa07909c45ce9f66686
nemanjamalesija <malesija13@gmail.com>
2023-07-20 01:57:04 +0200

    Add remove friend controller and route. Refactor

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -2,17 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import catchAsync from '../helpers/catchAsync.ts';
 import User from '../models/User.ts';
 import controllerFactory from './controllerFactory.ts';
-import { UserType } from '../types/User.ts';
 
 const getAllUsers = controllerFactory.getAll(User);
 const createUser = controllerFactory.createOne(User);
 
 const getOneUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    let activeUser: UserType;
-    let isAlreadyFriends = false;
-    let isFriendRequested = false;
-
     const targetUser = await User.findById(req.params.id)
       .populate({
         path: 'friends',
@@ -29,11 +24,11 @@ const getOneUser = catchAsync(
     }
 
     if (req.body.currentUser.id !== targetUser.id) {
-      isFriendRequested = targetUser.friendRequests.some(
+      const isFriendRequested = targetUser.friendRequests.some(
         (friend) => friend.id === req.body.currentUser.id
       );
 
-      isAlreadyFriends = targetUser.friends.some(
+      const isAlreadyFriends = targetUser.friends.some(
         (friend) => friend.id === req.body.currentUser.id
       );
 
@@ -58,30 +53,34 @@ const getOneUser = catchAsync(
 
 const addFriend = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const userToAdd = await User.findById(req.body.id);
-    const currentUser = await User.findById(req.body.currentUser.id);
+    const currentUserId = req.body.currentUser.id;
+    const targetUserId = req.body.id;
 
-    if (!userToAdd || !currentUser) {
+    const [currentUser, targetUser] = await User.find({
+      _id: { $in: [currentUserId, targetUserId] },
+    });
+
+    if (!targetUser || !currentUser) {
       res.status(404).json({ error: 'This user does not exist' });
       return next();
     }
 
-    if (userToAdd.id === currentUser.id) {
+    if (targetUser.id === currentUser.id) {
       res.status(404).json({ error: 'You cannot add yourself as a friend' });
       return next();
     }
 
     if (
-      userToAdd.friendRequests.includes(currentUser.id) ||
-      currentUser.friendRequests.includes(userToAdd.id)
+      targetUser.friendRequests.includes(currentUser.id) ||
+      currentUser.friendRequests.includes(targetUser.id)
     ) {
       res.status(400).json({ error: 'Already requested' });
       return next();
     }
 
     if (
-      userToAdd.friends.includes(currentUser.id) ||
-      currentUser.friends.includes(userToAdd.id)
+      targetUser.friends.includes(currentUser.id) ||
+      currentUser.friends.includes(targetUser.id)
     ) {
       res
         .status(400)
@@ -89,9 +88,9 @@ const addFriend = catchAsync(
       return next();
     }
 
-    userToAdd.friendRequests = [...userToAdd.friendRequests, currentUser.id];
+    targetUser.friendRequests = [...targetUser.friendRequests, currentUser.id];
 
-    await userToAdd.save({ validateBeforeSave: false });
+    await targetUser.save({ validateBeforeSave: false });
     res.status(200).json({
       status: 'success',
       message: 'Request sucessfully sent',
@@ -101,31 +100,35 @@ const addFriend = catchAsync(
 
 const acceptFriendRequest = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const userRequesting = await User.findById(req.body.id);
-    const currentUser = await User.findById(req.body.currentUser.id);
+    const currentUserId = req.body.currentUser.id;
+    const targetUserId = req.body.id;
 
-    if (!userRequesting || !currentUser) {
+    const [currentUser, targetUser] = await User.find({
+      _id: { $in: [currentUserId, targetUserId] },
+    });
+
+    if (!targetUser || !currentUser) {
       res.status(400).json({ error: 'This user does not exist' });
       return next();
     }
 
     if (
-      userRequesting.friends.includes(currentUser.id) ||
-      currentUser.friends.includes(userRequesting.id)
+      targetUser.friends.includes(currentUser.id) ||
+      currentUser.friends.includes(targetUser.id)
     ) {
       res.status(400).json({ error: 'You are already friend with this user.' });
       return next();
     }
 
-    currentUser.friends.push(userRequesting.id);
+    currentUser.friends.push(targetUser.id);
     currentUser.friendRequests = currentUser.friendRequests.filter(
-      (id) => id.toString() !== userRequesting.id
+      (id) => id.toString() !== targetUser.id
     );
 
-    userRequesting.friends.push(currentUser.id);
+    targetUser.friends.push(currentUser.id);
 
     await currentUser.save({ validateBeforeSave: false });
-    await userRequesting.save({ validateBeforeSave: false });
+    await targetUser.save({ validateBeforeSave: false });
 
     res.status(200).json({
       status: 'success',
@@ -136,16 +139,16 @@ const acceptFriendRequest = catchAsync(
 
 const denyFriendRequest = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const requestedUser = await User.findById(req.body.id);
     const currentUser = await User.findById(req.body.currentUser.id);
+    const targetUser = await User.findById(req.body.id);
 
-    if (!requestedUser || !currentUser) {
+    if (!targetUser || !currentUser) {
       res.status(400).json({ error: 'This user does not exist' });
       return next();
     }
 
     currentUser.friendRequests = currentUser.friendRequests.filter(
-      (id) => id.toString() !== requestedUser.id
+      (id) => id.toString() !== targetUser.id
     );
 
     await currentUser.save({ validateBeforeSave: false });
@@ -157,14 +160,14 @@ const denyFriendRequest = catchAsync(
   }
 );
 
-// const [sourceUser, targetUser] = await User.find({
-//   _id: { $in: [currentUser, id] },
-// });
-
 const removeFriend = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const currentUser = await User.findById(req.body.currentUser.id);
-    const targetUser = await User.findById(req.body.id);
+    const currentUserId = req.body.currentUser.id;
+    const targetUserId = req.body.id;
+
+    const [currentUser, targetUser] = await User.find({
+      _id: { $in: [currentUserId, targetUserId] },
+    });
 
     if (!targetUser || !currentUser) {
       res.status(400).json({ error: 'This user does not exist' });
