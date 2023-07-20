da2420fc86bd8e0092b11fecedc77a36661f1886
nemanjamalesija <malesija13@gmail.com>
2023-07-21 00:16:49 +0200

    Add errors utils. Add error controller

---
+++ server/controllers/errorController.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { HttpError } from '../utils/errors.ts';
+
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) => {
+  if (err instanceof HttpError)
+    return res.status(err.code).json({
+      error: {
+        code: err.code,
+        message: err.message,
+      },
+    });
+
+  console.log(err);
+
+  return res.status(500).json({
+    error: {
+      code: 500,
+      message: 'Something went wrong',
+    },
+  });
+};
+
+export default errorHandler;
