57c59564a44d96c4ed2696aebdf77ebb5715bf6a
nemanjamalesija <malesija13@gmail.com>
2023-07-21 21:14:04 +0200

    Setup client template

---
+++ client/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1 class="text-red-500 text-2xl">Hello</h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
