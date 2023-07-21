78c0043dd87266707495aa734339a07cfda29003
nemanjamalesija <malesija13@gmail.com>
2023-07-21 13:36:06 +0200

    Refactor comment controller with comment services

--- server/controllers/commentController.ts
+++ server/controllers/commentController.ts
@@ -1,75 +1,36 @@
 import catchAsync from '../helpers/catchAsync.ts';
-import { Request, Response, NextFunction } from 'express';
-import User from '../models/User.ts';
-import Post from '../models/Post.ts';
+import { Request, Response } from 'express';
 import Comment from '../models/Comment.ts';
 import controllerFactory from './controllerFactory.ts';
+import commentServices from '../services/commentServices.ts';
 
-const createComment = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const currentUserId = req.body.currentUser.id;
-    const targetPostId = req.params.id;
-    const newCommentText = req.body.text;
+const createComment = catchAsync(async (req: Request, res: Response) => {
+  const currentUserId = req.body.currentUser.id;
+  const targetPostId = req.params.id;
+  const newCommentText = req.body.text;
 
-    const currentUser = await User.findById(currentUserId);
-    const targetPost = await Post.findById(targetPostId);
+  const updatedPost = await commentServices.createComment(
+    currentUserId,
+    targetPostId,
+    newCommentText
+  );
 
-    if (!currentUser) {
-      res.status(400).json({ error: 'This user does not exist' });
-      return next();
-    }
+  res.status(200).json({
+    message: 'success',
+    data: { updatedPost },
+  });
+});
 
-    if (!targetPost) {
-      res.status(400).json({ error: 'This post does not exist' });
-      return next();
-    }
+const deleteComment = catchAsync(async (req: Request, res: Response) => {
+  const targetcommentId = req.params.id;
 
-    const newComment = await Comment.create({
-      text: newCommentText,
-      author: currentUserId,
-      post: targetPostId,
-    });
+  await commentServices.deleteComment(targetcommentId);
 
-    targetPost.comments.push(newComment.id);
-
-    await targetPost.save();
-
-    res.status(200).json({
-      message: 'success',
-      data: { newComment },
-    });
-  }
-);
-
-const deleteComment = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const targetcomment = await Comment.findById(req.params.id);
-
-    if (!targetcomment) {
-      res.status(400).json({ error: 'This comment does not exist' });
-      return next();
-    }
-
-    const targetPost = await Post.findById(targetcomment.post);
-
-    if (!targetPost) {
-      res.status(400).json({ error: 'This post does not exist' });
-      return next();
-    }
-
-    targetPost.comments = targetPost.comments.filter(
-      (id) => id.toString() !== targetcomment.id
-    );
-
-    await targetPost.save();
-    await Comment.findByIdAndUpdate(targetcomment.id);
-
-    res.status(204).json({
-      status: 'sucess',
-      data: null,
-    });
-  }
-);
+  res.status(204).json({
+    status: 'sucess',
+    data: null,
+  });
+});
 const editComment = controllerFactory.updateOne(Comment);
 
 export default { createComment, deleteComment, editComment };
---
+++ server/services/commentServices.ts
@@ -0,0 +1,56 @@
+import { AppError } from '../helpers/appError.ts';
+import Comment from '../models/Comment.ts';
+import Post from '../models/Post.ts';
+import User from '../models/User.ts';
+
+async function createComment(
+  currentUserId: string,
+  targetPostId: string,
+  newCommentText: string
+) {
+  const currentUser = await User.findById(currentUserId);
+  const targetPost = await Post.findById(targetPostId);
+
+  if (!currentUser) {
+    throw new AppError('User not found', 404);
+  }
+
+  if (!targetPost) {
+    throw new AppError('Post not found', 404);
+  }
+
+  const newComment = await Comment.create({
+    text: newCommentText,
+    author: currentUserId,
+    post: targetPostId,
+  });
+
+  targetPost.comments.push(newComment.id);
+
+  await targetPost.save();
+
+  return targetPost;
+}
+
+async function deleteComment(targetPostId: string) {
+  const targetcomment = await Comment.findById(targetPostId);
+
+  if (!targetcomment) {
+    throw new AppError('Comment not found', 404);
+  }
+
+  const targetPost = await Post.findById(targetcomment.post);
+
+  if (!targetPost) {
+    throw new AppError('Post not found', 404);
+  }
+
+  targetPost.comments = targetPost.comments.filter(
+    (id) => id.toString() !== targetcomment.id
+  );
+
+  await targetPost.save();
+  await Comment.findByIdAndUpdate(targetcomment.id);
+}
+
+export default { createComment, deleteComment };
