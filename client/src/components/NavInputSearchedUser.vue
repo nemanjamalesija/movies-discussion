871afd3d1704be7a6615c1a85bec1aedfd32b751
nemanjamalesija <malesija13@gmail.com>
2023-08-03 05:50:14 +0200

    Display user when searched

---
+++ client/src/components/NavInputSearchedUser.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { UserType } from '@/types/userType'
+import UserPhotoAndName from './ui/UserPhotoAndName.vue'
+
+const props = defineProps<{ user: UserType }>()
+</script>
+<template>
+  <div class="searched-user">
+    <UserPhotoAndName
+      containerClass="flex gap-3 items-center rounded-t-md cursor-pointer "
+      :currentUser="props.user"
+      :wrapperSize="{ height: '2.5rem', width: '2.5rem' }"
+      :imageSize="{ height: '2rem', width: '2rem' }"
+    >
+      <template #user-photo-adjacent>
+        <div class="flex flex-col gap-1">
+          <h2 class="flex items-center gap-1 font-semibold text-sm">
+            <span>{{ props.user.firstName }}</span>
+            <span>{{ props.user.lastName }}</span>
+          </h2>
+        </div>
+      </template>
+    </UserPhotoAndName>
+  </div>
+</template>
+
+<style scoped>
+.searched-user:not(:last-child) {
+  margin-bottom: 0.5rem;
+}
+</style>
--- client/src/components/ui/SearchUserInput.vue
+++ client/src/components/ui/SearchUserInput.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import useAppNavigation from '@/composables/useAppNavigation'
-import { baseUrl } from '@/constants/baseUrl'
-import type { UserType } from '@/types/userType'
+import useAppNavigation from '../../composables/useAppNavigation'
+import { baseUrl } from '../../constants/baseUrl'
+import type { UserType } from '../../types/userType'
 import { ref } from 'vue'
+import LoadingSpinner from '../LoadingSpinner.vue'
+import NavInputSearchedUser from '../NavInputSearchedUser.vue'
 
 const tryUser = ref<string>()
-const searchedUsers = ref({} as UserType[])
+const searchedUsers = ref([] as UserType[])
 const loadingUsers = ref<boolean>(false)
 const { toast, router } = useAppNavigation()
 
 async function getSearchedUSers() {
   const jwtToken = localStorage.getItem('jwt')
 
-  if (!tryUser.value) return
+  if (!tryUser.value) {
+    searchedUsers.value = []
+    return
+  }
 
   const [firstName, lastName] = tryUser.value.split(' ')
 
@@ -46,7 +51,6 @@ async function getSearchedUSers() {
       } = await response.json()
 
       searchedUsers.value = users
-      console.log(searchedUsers)
       loadingUsers.value = false
     }
   } catch (error) {
@@ -59,32 +63,48 @@ async function getSearchedUSers() {
 }
 </script>
 <template>
-  <label for="default-search" class="mb-2 text-sm font-medium sr-only">Search</label>
-  <div class="relative">
-    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-      <svg
-        class="w-4 h-4 text-slate-500"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 20 20"
-      >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+  <div class="flex flex-col relative">
+    <div>
+      <label for="default-search" class="mb-2 text-sm font-medium sr-only">Search</label>
+      <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            class="w-4 h-4 text-slate-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          class="block w-full py-[0.5rem] px-6 pl-10 text-sm border border-slate-300 rounded-full bg-slate-50 focus:ring-indigo-500 focus:border-indigo-500 outline-none"
+          placeholder="Search"
+          v-model="tryUser"
+          @input="getSearchedUSers"
         />
-      </svg>
+      </div>
     </div>
-    <input
-      type="search"
-      id="default-search"
-      class="block w-full py-[0.5rem] px-6 pl-10 text-sm border border-slate-300 rounded-full bg-slate-50 focus:ring-indigo-500 focus:border-indigo-500 outline-none"
-      placeholder="Search"
-      v-model="tryUser"
-      @input="getSearchedUSers"
-    />
+    <div class="bg-white absolute top-[105%] left-0 w-full rounded-md shadow-lg p-3">
+      <LoadingSpinner v-if="loadingUsers" />
+      <p
+        v-if="searchedUsers?.length === 0 && !loadingUsers"
+        class="text-slate-500 text-base text-center"
+      >
+        No results found
+      </p>
+      <template v-else>
+        <NavInputSearchedUser v-for="user in searchedUsers" :key="user._id" :user="user" />
+      </template>
+    </div>
   </div>
 </template>
