e4704d65d9a5011803d86546445d81c6b2dcb18f
nemanjamalesija <malesija13@gmail.com>
2023-08-14 03:18:46 +0200

    Refactor post comments number to separate component

---
+++ client/src/components/PostCommentsInfo.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { CommentType } from '../types/postType'
+import type { Ref } from 'vue'
+
+const props = defineProps<{
+  comments: Ref<CommentType[]>
+}>()
+</script>
+<template>
+  <div>
+    <p
+      role="button"
+      tabindex="0"
+      class="flex items-center gap-1 text-slate-500 text-sm"
+      @click="$emit('toggleComments')"
+    >
+      <span>{{ props.comments.value.length }}</span>
+      <span
+        >{{
+          props.comments.value.length > 1 || props.comments.value.length === 0
+            ? 'Comments'
+            : 'Comment'
+        }}
+      </span>
+    </p>
+  </div>
+</template>
